c65a0451883e15c463afb00b44e65b9611c0b202
Aitor <aitor@max12>
2025-11-05 10:42:56 +0100

    Creación test Sign Up

--- test/crudbankclientsideapplication/ui/SignUpControllerTest.java
+++ test/crudbankclientsideapplication/ui/SignUpControllerTest.java
@@ -5,8 +5,19 @@
  */
 package crudbankclientsideapplication.ui;
 
+import crudbankclientsideapplication.CRUDBankClientSideApplication;
+import javafx.stage.Stage;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.FixMethodOrder;
+import org.junit.runners.MethodSorters;
+import static org.testfx.api.FxAssert.verifyThat;
+import org.testfx.framework.junit.ApplicationTest;
+import static org.testfx.matcher.base.NodeMatchers.isDisabled;
+import static org.testfx.matcher.base.NodeMatchers.isEnabled;
+import static org.testfx.matcher.base.NodeMatchers.isFocused;
+import static org.testfx.matcher.base.NodeMatchers.isVisible;
+import static org.testfx.matcher.control.TextInputControlMatchers.hasText;
 
 /**
  *
